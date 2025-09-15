c54bec3457bfe25e827b4f5f70da2c133f5ce256
H11iye <hamdi.iyed11@gmail.com>
2025-09-15 14:43:33 +0000

    Update routes file

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,16 @@
-from flask import jsonify, request
-from .services import get_crypto_price, get_historical_data
+# app/routes.py
+from flask import jsonify
+from .service import get_crypto_price
 
 def register_routes(app):
-    @app.route('/')
+    @app.route("/")
     def home():
-        return ({"message": "Welcome to Binance Crypto Assets API!"})
-    
+        return jsonify({"message": "Binance Crypto API is running!"})
+
     @app.route("/crypto/<symbol>")
     def crypto_price(symbol):
         try:
             price = get_crypto_price(symbol)
             return jsonify({"symbol": symbol.upper(), "price": price, "currency": "USDT"})
-        except ValueError as e:
+        except Exception as e:
             return jsonify({"error": str(e)}), 400
-        
-        
