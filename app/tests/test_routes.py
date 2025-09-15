b6a12bf6f24ff085a88d8e01008cdae031a80dff
H11iye <hamdi.iyed11@gmail.com>
2025-09-15 14:48:14 +0000

    Update the test_routes file

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -1,8 +1,18 @@
+# app/tests/test_routes.py
 from app.main import create_app
 
 def test_home():
     app = create_app()
     client = app.test_client()
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
-    assert response.json == {"message": "Crypto Assets API is running."}
+    assert "Binance Crypto API" in response.get_json()["message"]
+
+def test_crypto_endpoint():
+    app = create_app()
+    client = app.test_client()
+    response = client.get("/crypto/BTC")
+    assert response.status_code == 200
+    data = response.get_json()
+    assert "price" in data
+    assert data["symbol"] == "BTC"
