c5809ac59f57396f93bfb87186b6e1a312e48ed9
H11iye <hamdi.iyed11@gmail.com>
2025-10-02 15:44:52 +0000

    Prepare resources for Cloud SQL for a private services connection between the VPC and google's managed service network

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,6 +84,23 @@ resource "google_project_iam_member" "n8n_sql_client" {
 # Cloud SQL (Postgres for n8n)
 #####################
 
+resource "google_compute_network" "default" {
+  name = "default"
+}
+
+resource "google_compute_global_address" "private_ip_range" {
+  name = "private-ip-range"
+  purpose = "VPC_PEERING"
+  address_type = "INTERNAL"
+  prefix_length = 16
+  network = google_compute_network.default.id
+}
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network = google_compute_network.default.self_link
+  service = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_range.name]
+}
+
 resource "google_sql_database_instance" "n8n_db_instance" {
   name = "${var.App_name}-n8n-db"
   region = var.REGION
@@ -93,7 +110,7 @@ resource "google_sql_database_instance" "n8n_db_instance" {
     tier = "db-f1-micro"
     ip_configuration {
       ipv4_enabled = false
-      private_network = google_compute_network.vpc.self_link
+      private_network = google_compute_network.default.id
       ssl_mode = "ENCRYPTED_ONLY"
 
     }
